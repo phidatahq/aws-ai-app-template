4554c0852a39a45953cca21021b9cd70830d390c
Ashpreet Bedi <ashpreet@phidata.com>
2023-04-05 13:00:00 -0400

    update

--- workspace/dev/postgres.py
+++ workspace/dev/postgres.py
@@ -7,6 +7,7 @@ from workspace.settings import ws_settings
 # Dev postgres: A postgres instance to use for dev data
 dev_postgres_app = PostgresDb(
     name=f"postgres-{ws_settings.ws_name}",
+    enabled=ws_settings.dev_postgres_enabled,
     db_user=ws_settings.ws_name,
     db_password=ws_settings.ws_name,
     db_schema=ws_settings.ws_name,
